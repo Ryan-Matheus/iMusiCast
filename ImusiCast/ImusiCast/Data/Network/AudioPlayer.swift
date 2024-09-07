bf42947b0cf8caa6191e4fb7b44a1cbcbc8d27a5
Ryan Matheus <ryanpqt266@gmail.com>
2024-09-07 19:58:54 -0300

    [ADD] added audio player

---
+++ ImusiCast/ImusiCast/Data/Network/AudioPlayer.swift
@@ -0,0 +1,36 @@
+import Foundation
+import AVFoundation
+
+class AudioPlayer: ObservableObject {
+    private var player: AVPlayer?
+    @Published var isPlaying: Bool = false
+    @Published var currentTime: TimeInterval = 0
+    @Published var duration: TimeInterval = 0
+    
+    func play(url: URL) {
+        let playerItem = AVPlayerItem(url: url)
+        player = AVPlayer(playerItem: playerItem)
+        player?.play()
+        isPlaying = true
+        
+        // Observe player status
+        player?.addPeriodicTimeObserver(forInterval: CMTime(seconds: 1, preferredTimescale: 1), queue: .main) { [weak self] time in
+            self?.currentTime = time.seconds
+            self?.duration = playerItem.duration.seconds
+        }
+    }
+    
+    func pause() {
+        player?.pause()
+        isPlaying = false
+    }
+    
+    func resume() {
+        player?.play()
+        isPlaying = true
+    }
+    
+    func seek(to time: TimeInterval) {
+        player?.seek(to: CMTime(seconds: time, preferredTimescale: 1))
+    }
+}
